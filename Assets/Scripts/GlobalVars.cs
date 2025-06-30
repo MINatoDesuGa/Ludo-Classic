fd34c51ca3a12469d2dcc0d2876ac83e596822b6
Lamminlun Simte <lminlunsims14@gmail.com>
2025-06-30 11:01:41 +0530

    setted up diceHolder, dice

---
+++ Assets/Scripts/GlobalVars.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GlobalVars : MonoBehaviour
+{
+    public static GlobalVars Instance;
+
+    public RectTransform CanvasRectTransform;
+    //======================================================//
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        } else {
+            Destroy(this);
+        }
+    }
+}
